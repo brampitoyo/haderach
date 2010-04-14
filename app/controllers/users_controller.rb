9c03b56b6b0b6736e9d3493d00fe3aee721302d1
mettadore <john@mettadore.com>
2010-04-13 19:54:14 -0700

    More login necessities, disallow anonymous user creation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,16 @@
 class UsersController < ApplicationController
+  before_filter :login_required, :except => [:index, :show]
+  
   def new
-    @user = User.new
+    if current_user.id == 1
+      @user = User.new
+    end
   end
   
   def create
-    @user = User.new(params[:user])
+    if current_user.id == 1
+      @user = User.new(params[:user])
+    end
     if @user.save
       flash[:notice] = "Thank you for signing up! You are now logged in."
       redirect_to root_url
